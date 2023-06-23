688c3b08bc0270f0570241672834cfe2e316fed7
ekkamon <ekkb.contact@gmail.com>
2023-06-24 01:08:22 +0700

    !hotfix: repair sign-in file.

--- src/services/sign-in.ts
+++ src/services/sign-in.ts
@@ -3,6 +3,7 @@ import { SignInServicePropsInterface } from "@/interfaces/SignInServicePropsInte
 import { SignInServiceResponseInterface } from "@/interfaces/SignInServiceResponseInterface";
 import axios from "axios";
 import crypto from "crypto";
+import CORSProxy from "./CORSProxy";
 import { RSADecrypt } from "@/utils/RSA";
 
 const encodeString = (str: string) => {
@@ -23,7 +24,7 @@ const SignInService = (props: SignInServicePropsInterface) => {
   );
 
   return axios.post<SignInServiceResponseInterface>(
-    "https://myapi.ku.th/auth/login",
+    CORSProxy("https://myapi.ku.th/auth/login"),
     {
       username: encodeString(props.username),
       password: encodeString(props.password),
@@ -32,20 +33,20 @@ const SignInService = (props: SignInServicePropsInterface) => {
       headers: {
         authority: "myapi.ku.th",
         accept: "*/*",
-        "accept-language": "th-TH,th;q=0.9,en-US;q=0.8,en;q=0.7",
+        "accept-language": "en-US,en;q=0.9,th-TH;q=0.8,th;q=0.7",
         "app-key": "txCR5732xYYWDGdd49M3R19o1OVwdRFc",
         "content-type": "application/json",
         origin: "https://my.ku.th",
         referer: "https://my.ku.th/",
         "sec-ch-ua":
-          '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+          '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
+        "sec-ch-ua-platform": '"macOS"',
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-site",
         "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
       },
     }
   );
