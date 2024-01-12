3a493c9f28261eebc1a8353bf45ac5a67a309e15
Teerut Srithongdee <teerutsr@gmail.com>
2024-01-12 16:30:52 +0700

    Update analytics scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,16 +15,8 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100;200;300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"
                 />
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZE0QMZN3Q"></script>
-                <script>
-                    {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'G-YZE0QMZN3Q');
-                   `}
-                </script>
+               <script defer src="https://eu.umami.is/script.js" data-website-id="81607876-b154-4ac9-aaf1-062fb84ff756"></script>
+               <script async src="https://umami.teerut.me/script.js" data-website-id="8c1f071c-63e3-4562-8b89-af9ed10e13d5"></script>
             </Head>
             <body>
                 <Main />
