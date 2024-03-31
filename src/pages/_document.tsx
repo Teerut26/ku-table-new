1d4fd0f19c0857aa7749981f2e738345c3ac6f9d
Teerut Srithongdee <46746401+Teerut26@users.noreply.github.com>
2024-04-01 00:19:56 +0700

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,11 +17,13 @@ export default function Document() {
                 />
                 <script async src="https://eu.umami.is/script.js" data-website-id="ae99cf5b-4930-4e72-8bf6-209130e00cbc"></script>
                 <script type="text/javascript">
+                    {`
                     (function(c,l,a,r,i,t,y){
                         c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                     })(window, document, "clarity", "script", "lpdophdw4g");
+                    `}
                 </script>
             </Head>
             <body>
